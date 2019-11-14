d216ce95169daad3e89a9e6bbe10b6ffbe6d08b7
SaroshKhawar496 <sarosh.496@gmail.com>
2019-11-14 15:23:20 -0800

    Setup for cloud deployment

---
+++ public/precache-manifest.f81b55f28b462065fc9c6cc1b5902d6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b97b3b783b35cf1dbd9e935b8ab8ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d00a0b70fd4414c6152d",
+    "url": "/static/css/main.6e97ea33.chunk.css"
+  },
+  {
+    "revision": "b843bb4e57e2b95ee022",
+    "url": "/static/js/2.da8ae454.chunk.js"
+  },
+  {
+    "revision": "d00a0b70fd4414c6152d",
+    "url": "/static/js/main.a1bdf573.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
