8b093a70f45b60a0258d33e4b82bf7e4f23a55e7
SaroshKhawar496 <sarosh.496@gmail.com>
2019-11-14 15:40:31 -0800

    fix 3 for route issues

--- server.js
+++ server.js
@@ -6,12 +6,6 @@ const app = express();
 
 app.use(cors());
 
-// deployment to Heroku
-app.use(express.static("public"));
-app.get("/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
-});
-
 app
   .get("/api/comic/latest", (req, response) => {
     let data;
@@ -39,6 +33,12 @@ app
         response.status(400).send({});
       });
   });
+
+// deployment to Heroku
+app.use(express.static("public"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
