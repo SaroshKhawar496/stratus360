56c1599d06fcf3c92bf42f4c50beb5b55818d05f
SaroshKhawar496 <sarosh.496@gmail.com>
2019-11-14 15:06:13 -0800

    Assessment Done

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+app.use(cors());
+
+app
+  .get("/api/comic/latest", (req, response) => {
+    let data;
+    // console.log("get triggered");
+    axios
+      .get("http://xkcd.com/info.0.json")
+      .then(res => {
+        response.send(res.data);
+        // console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+        response.status(400).send({});
+      });
+  })
+  .get("/api/comic/:comicId", (req, response) => {
+    let data;
+    axios
+      .get(`http://xkcd.com/${req.params.comicId}/info.0.json`)
+      .then(res => {
+        response.send(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+        response.status(400).send({});
+      });
+  });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
